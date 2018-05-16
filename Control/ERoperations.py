ccd23be4705465278a4f26dde2ebe9dc47d8c449
Fabio Moreira <fabiomoreira.mf@gmail.com>
2018-05-15 22:41:43 -0300

    De Simone
    Continuação da implementação De Simone

--- Control/ERoperations.py
+++ Control/ERoperations.py
@@ -26,11 +26,13 @@ class ERoperations:
                 else:
                     print(nodo.get_simbolo() + " pai: " + nodo.get_pai().get_simbolo() + " lado: " + nodo.get_lado_pai() + " não costurado")
             else:
-                print(nodo.get_simbolo() + " raiz")
-        return
+                if nodo.get_eh_raiz() is not None:
+                    print(nodo.get_simbolo() + " raiz" + " costurado com:" + nodo.get_costura().get_simbolo())
+                else:
+                    print(nodo.get_simbolo() + " raiz")
 
         # Criar tabela
-        ERoperations.criar_tabela(arvore)
+        ERoperations.criacao_tabela(arvore)
 
     @staticmethod
     def costurar_arvore(pilha, elemento):
@@ -53,7 +55,7 @@ class ERoperations:
             new_er_expression, operador_atual = ERoperations.remover_parenteses(new_er_expression)
             operador_atual, posicao_operador_atual = ERoperations.new_operator(operador_atual, new_er_expression)
 
-        nodo_raiz = Nodo(operador_atual, None, None, True, False)
+        nodo_raiz = Nodo(operador_atual, None, None, False, True)
         arvore.insert(len(arvore), nodo_raiz)
 
         if operador_atual == '.' or operador_atual == '|':
@@ -69,31 +71,89 @@ class ERoperations:
             nodo_raiz.set_filho_esquerda(nodo_esq)
 
     @staticmethod
-    def criar_tabela(elemento, direcao):
-        if elemento.get_eh_folha():
-            pass
+    def criacao_tabela(arvore):
+        number_states = 1
+        state = 'Q'
+
+        novos_estados = []
+        historico = []
+        lista_estados_atual = []
+        dict_associacao = {}
+        dict_simbolo_estado = {}
+        dict_af = {}
+        ja_add = []
+
+        # Estado inicial
+        ERoperations.criar_tabela(lista_estados_atual, arvore[0], 'DESCER')
+        dict_af['Q0'] = []
+        for simbolos in lista_estados_atual:
+            if simbolos.get_simbolo() not in ja_add and simbolos.get_simbolo() != 'λ':
+                novo_estado = state + str(number_states)
+                dict_af['Q0'].insert(len(dict_af['Q0']), [simbolos.get_simbolo(), novo_estado])
+                novos_estados.insert(len(novos_estados), novo_estado)
+                dict_associacao[novo_estado] = [simbolos]
+                dict_simbolo_estado[simbolos.get_simbolo()] = [novo_estado]
+                number_states += 1
+                ja_add.insert(0, simbolos.get_simbolo())
+            elif simbolos.get_simbolo() != 'λ':
+                dict_associacao[dict_simbolo_estado[simbolos.get_simbolo()]].insert(0, simbolos)
+        historico = lista_estados_atual.copy()
+
+        # print(novos_estados)
+        # print(dict_af)
+        # print(dict_simbolo_estado)
+        # print(dict_associacao)
+        # for s in lista_estados_atual:
+        #     print(s.get_simbolo())
+        # return
+
+        while len(novos_estados) != 0:
+            ja_add = []
+            estado_atual = novos_estados.pop(0)
+            lista_estados_atual = []
+
+            for simbolos in dict_associacao[estado_atual]:
+                ERoperations.criar_tabela(lista_estados_atual, simbolos.get_costura(), 'SUBIR')
+
+            dict_af[estado_atual] = []
+            for simbolos in lista_estados_atual:
+                if simbolos.get_simbolo() not in ja_add:
+                    novo_estado = state+str(number_states)
+                    dict_af[estado_atual].insert(len(dict_af[novo_estado]), [simbolos.get_simbolo(), novo_estado])
+                    novos_estados.insert(len(novos_estados), novo_estado)
+                    dict_associacao[novo_estado] = [simbolos]
+                    dict_simbolo_estado[simbolos.get_simbolo()] = [novo_estado]
+                    number_states += 1
+                    ja_add.insert(0, simbolos.get_simbolo())
+                else:
+                    dict_associacao[dict_simbolo_estado[simbolos.get_simbolo()]].insert(0, simbolos)
+
+    @staticmethod
+    def criar_tabela(lista_elementos, elemento, direcao):
+        if elemento.get_eh_folha() or elemento.get_simbolo() == 'λ':
+            lista_elementos.insert(len(lista_elementos), elemento)
 
         if elemento.get_simbolo() == '*':
-            ERoperations.criar_tabela(elemento.get_filho_esquerda(), 'DESCER')
-            ERoperations.criar_tabela(elemento.get_costura(), 'SUBIR')
+            ERoperations.criar_tabela(lista_elementos, elemento.get_filho_esquerda(), 'DESCER')
+            ERoperations.criar_tabela(lista_elementos, elemento.get_costura(), 'SUBIR')
         if elemento.get_simbolo() == '.':
             if direcao == 'DESCER':
-                ERoperations.criar_tabela(elemento.get_filho_esquerda(), 'DESCER')
+                ERoperations.criar_tabela(lista_elementos, elemento.get_filho_esquerda(), 'DESCER')
             else:
-                ERoperations.criar_tabela(elemento.get_filho_esquerda(), 'DESCER')
+                ERoperations.criar_tabela(lista_elementos, elemento.get_filho_esquerda(), 'DESCER')
         if elemento.get_simbolo() == '|':
             if direcao == 'DESCER':
-                ERoperations.criar_tabela(elemento.get_filho_esquerda(), 'DESCER')
-                ERoperations.criar_tabela(elemento.get_filho_direita(), 'DESCER')
+                ERoperations.criar_tabela(lista_elementos, elemento.get_filho_esquerda(), 'DESCER')
+                ERoperations.criar_tabela(lista_elementos, elemento.get_filho_direita(), 'DESCER')
             else:
                 tmp_elemento = elemento.get_filho_direita()
                 while tmp_elemento.get_costura() is None:
                     tmp_elemento = tmp_elemento.get_filho_direita()
-                ERoperations.criar_tabela(tmp_elemento.get_costura(), 'SUBIR')
+                ERoperations.criar_tabela(lista_elementos, tmp_elemento.get_costura(), 'SUBIR')
         if elemento.get_simbolo() == '?':
             if direcao == 'DESCER':
-                ERoperations.criar_tabela(elemento.get_filho_esquerda(), 'DESCER')
-            ERoperations.criar_tabela(elemento.get_costura(), 'SUBIR')
+                ERoperations.criar_tabela(lista_elementos, elemento.get_filho_esquerda(), 'DESCER')
+            ERoperations.criar_tabela(lista_elementos, elemento.get_costura(), 'SUBIR')
 
 
     @staticmethod
